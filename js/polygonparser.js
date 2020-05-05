59b7c9a22ffb77210d85eee3ab55018d2ea235a9
AvdylKrasniqi <iamavdylk@gmail.com>
2020-05-05 23:39:18 +0200

    ngjyra ne baz te sa pem i ka

--- js/polygonparser.js
+++ js/polygonparser.js
@@ -13,6 +13,7 @@ class PolygonParser {
         for(let i = 0; i < pemet.length; i++){
             this.krijoPemet(pemet[i]);
         }
+        setTimeout(function(){}, 0);
         this.render();
     }
     set color(color){
@@ -41,7 +42,13 @@ class PolygonParser {
     krijoPemet(jsonUrl) {
         this.pemet.push(new TreeParser(jsonUrl));
     }
-
+    numriPemve(){
+        let sum = 0;
+        for(var i = 0; i < this.pemet.length; i++){
+            sum += this.pemet[i].totalNumberOfTrees;
+        }
+        return sum;
+    }
     get pemet() {
         return this.pemet;
     }
@@ -52,10 +59,21 @@ class PolygonParser {
             console.log(_this.polygonName);
             _this.polygoncoords =  data[0][_this.polygonName];
             _this.turfpolygon = turf.polygon([data[0][_this.polygonName]]);
-            _this.layer = L.polygon(_this.polygoncoords, {color: "green"}).bindTooltip(_this.polygonName,
+            _this.layer = L.polygon(_this.polygoncoords, {color: "white", fillColor: "green", fillOpacity: 0.8, opacity: 0.8}).bindTooltip(_this.polygonName,
                 {permanent: false, direction:"center"}
             ).openTooltip().addTo(map);
         });
+        setTimeout(function(){
+            let color;
+            let nrPemve = _this.numriPemve();
+            if(nrPemve < 1000) color = "#7fd67f";
+            else if(nrPemve < 1000) color = "#47aa47";
+            else if(nrPemve < 1500) color = "#279027";
+            else if(nrPemve < 2500) color = "#177917";
+            else  color = "darkgreen";
+            _this.layer.setStyle({"fillColor": color});
+            console.log(nrPemve);
+        }, 3000);
     }
     infoParser(){
 
